d3337cbbf897493fe08e7b352bb72aaed75af5d4
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2020-11-09 15:42:01 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+q1 = input("Would you like to convert a letter or a word?: ")
+
+
+def letter_calc():
+    dictionary = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26
+    }
+    print(dictionary[str(input("Enter a letter: "))])
+
+
+def word_calc():
+    dictionary = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26
+    }
+    print(w1)
+    print(dictionary[str(input("Enter a letter: "))])
+
+
+if q1 == "letter":
+    letter_calc()
+
+if q1 == "word":
+    w1 = input("Enter a word: ")
+    w2 = list(w1)
+    w3 = len(w2)
+    for i in range(w3):
+        word_calc()
+
